60f911f09420753b22509900ab0a57303c105eb5
Yannick Baron <yannick.baron@thinktecture.com>
2021-06-17 10:35:16 +0200

    feat: add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {}), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
