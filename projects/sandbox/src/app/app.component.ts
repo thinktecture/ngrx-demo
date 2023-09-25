1981685c0799ce7b10c49cfe68bc93b6776bb283
Yannick Baron <yannick.baron@thinktecture.com>
2023-09-25 15:46:13 +0200

    feat(sandbox): add counter

--- projects/sandbox/src/app/app.component.ts
+++ projects/sandbox/src/app/app.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, CounterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'sandbox';
-}
+export class AppComponent {}
---
+++ projects/sandbox/src/app/counter/counter.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, effect, signal } from '@angular/core';
+
+let id = 1;
+
+function colorPicker(colors: string[]): (n: number) => string {
+  const numColors = colors.length;
+  return n => colors[n % numColors];
+}
+
+const nthColor = colorPicker(['#9cd08f', '#21295c', '#8b8bae', '#af125a', '#582b11']);
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss'],
+})
+export class CounterComponent {
+  private readonly storageKey = `counter${id++}`;
+  private readonly restored = parseInt(sessionStorage.getItem(this.storageKey) ?? '');
+
+  readonly counter = signal(this.restored || 0);
+
+  color = computed(() => {
+    console.log(this.storageKey, 'running');
+
+    return nthColor(this.counter());
+  });
+
+  increment(): void {
+    this.counter.update(count => ++count);
+  }
+
+  reset(): void {
+    this.counter.set(0);
+  }
+
+  readonly persist = effect(() => {
+    console.log('persisting', this.storageKey);
+
+    sessionStorage.setItem(this.storageKey, `${this.counter()}`);
+  });
+}
