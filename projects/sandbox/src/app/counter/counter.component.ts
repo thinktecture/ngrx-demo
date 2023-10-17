2c315bc9669adab67f921ed77dda2fcf77adc1e5
Yannick Baron <yannick.baron@thinktecture.com>
2023-10-17 20:42:08 +0200

    demo: late start

--- projects/sandbox/src/app/counter/counter.component.ts
+++ projects/sandbox/src/app/counter/counter.component.ts
@@ -1,5 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  EffectRef,
+  inject,
+  Injector,
+  Input,
+  signal,
+} from '@angular/core';
 
 let id = 1;
 
@@ -18,10 +27,20 @@ const nthColor = colorPicker(['#9cd08f', '#21295c', '#8b8bae', '#af125a', '#582b
   styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent {
-  private readonly storageKey = `counter${id++}`;
-  private readonly restored = parseInt(sessionStorage.getItem(this.storageKey) ?? '');
+  private readonly injector = inject(Injector);
+  private effectRef?: EffectRef;
 
-  readonly counter = signal(this.restored || 0);
+  @Input({ required: true }) storageKey = '';
+
+  readonly counter = signal(0);
+
+  ngOnInit(): void {
+    const restored = parseInt(sessionStorage.getItem(this.storageKey) ?? '');
+
+    this.counter.set(restored || 0);
+
+    this.storeCounter();
+  }
 
   color = computed(() => {
     console.log(this.storageKey, 'running');
@@ -37,9 +56,23 @@ export class CounterComponent {
     this.counter.set(0);
   }
 
-  readonly persist = effect(() => {
-    console.log('persisting', this.storageKey);
+  start(): void {
+    this.storeCounter();
+  }
 
-    sessionStorage.setItem(this.storageKey, `${this.counter()}`);
-  });
+  stop(): void {
+    this.effectRef?.destroy();
+  }
+
+  private storeCounter(): void {
+    this.effectRef?.destroy();
+    this.effectRef = effect(
+      () => {
+        console.log('persisting', this.storageKey);
+
+        sessionStorage.setItem(this.storageKey, `${this.counter()}`);
+      },
+      { injector: this.injector },
+    );
+  }
 }
