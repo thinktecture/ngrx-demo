2bf84ab045cb9ca32ffa9c89bbd0bbd3d7ea73ca
Yannick Baron <yannick.baron@thinktecture.com>
2024-02-17 16:50:59 +0900

    refactor(todo-service): use signal store

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,15 +1,16 @@
-import { Injectable } from '@angular/core';
+import { patchState, signalStore, type, withHooks, withMethods, withState } from '@ngrx/signals';
+import { addEntity, setAllEntities, updateEntity, withEntities } from '@ngrx/signals/entities';
 import { Observable, of, throwError } from 'rxjs';
 import { TodoList, TodoListItem, TodoListName } from './todo.model';
 
-export let LISTS: TodoListName[] = [
+export const LISTS: TodoListName[] = [
   { id: 'tl-1', title: 'First TODO List' },
   { id: 'tl-2', title: 'other List' },
 ];
 
 type StoredListItem = TodoListItem & { listId: string };
 
-export let ITEMS: StoredListItem[] = [
+export const ITEMS: StoredListItem[] = [
   { listId: 'tl-1', id: 'tl-1+1', content: 'Item 1 (L 1)', done: false },
   { listId: 'tl-1', id: 'tl-1+2', content: 'Item 2 (L 1)', done: true },
   { listId: 'tl-1', id: 'tl-1+3', content: 'Item 3 (L 1)', done: false },
@@ -20,45 +21,64 @@ export function getItems(listId: string): TodoListItem[] {
   return ITEMS.filter(item => item.listId === listId).map(({ listId, ...item }) => item);
 }
 
-@Injectable({
-  providedIn: 'root',
-})
-export class TodoService {
-  getListNames(): Observable<TodoListName[]> {
-    return of(LISTS);
-  }
+const lists = { entity: type<TodoListName>(), collection: 'lists' } as const;
+const items = { entity: type<TodoListItem & { listId: string }>(), collection: 'items' } as const;
 
-  getList(id: string): Observable<TodoList> {
-    const foundList = LISTS.find(list => list.id === id);
-    if (foundList) {
-      const items = getItems(id);
-      return of({ ...foundList, items });
-    }
-    return throwError(() => `Cannot find list with id ${id}`);
-  }
+export const TodoService = signalStore(
+  { providedIn: 'root' },
 
-  addList(title: string): Observable<TodoListName> {
-    const list: TodoListName = { id: `tl-${Date.now()}`, title };
-    LISTS = [...LISTS, list];
-    return of(list);
-  }
+  withState({}),
 
-  addItem(listId: string, content: string): Observable<TodoListItem> {
-    const item: StoredListItem = { listId, id: `${listId}+${Date.now()}`, content, done: false };
-    ITEMS = [...ITEMS, item];
-    return of(item);
-  }
+  withEntities(lists),
+  withEntities(items),
 
-  updateItem(listId: string, id: string, update: Partial<TodoListItem>): Observable<TodoListItem> {
-    const foundItem = ITEMS.find(item => item.id === id);
-    if (foundItem) {
-      if (update.content === '') {
+  withMethods(store => ({
+    getListNames(): Observable<TodoListName[]> {
+      return of(store.listsEntities());
+    },
+
+    getList(id: string): Observable<TodoList> {
+      const list = store.listsEntityMap()[id];
+
+      if (list) {
+        const items = store.itemsEntities().filter(item => item.listId === id);
+        return of({ ...list, items });
+      }
+
+      return throwError(() => `Cannot find list with id ${id}`);
+    },
+
+    addList(title: string): Observable<TodoListName> {
+      const list: TodoListName = { id: `tl-${Date.now()}`, title };
+      patchState(store, addEntity(list, lists));
+      return of(list);
+    },
+
+    addItem(listId: string, content: string): Observable<TodoListItem> {
+      const item: TodoListItem = { id: `${listId}+${Date.now()}`, content, done: false };
+      patchState(store, addEntity({ ...item, listId }, items));
+      return of(item);
+    },
+
+    updateItem(
+      listId: string,
+      id: string,
+      changes: Partial<TodoListItem>,
+    ): Observable<TodoListItem> {
+      if (changes.content === '') {
         return throwError(() => `Invalid content`);
       }
-      const newItem = { ...foundItem, ...update, id };
-      ITEMS = ITEMS.map(item => (item.id === id && item.listId === listId ? newItem : item));
-      return of(newItem);
-    }
-    return throwError(() => `Cannot locate Item`);
-  }
-}
+      if (!store.listsEntityMap()[listId] || !store.itemsEntityMap()[id]) {
+        return throwError(() => `Cannot locate Item`);
+      }
+      patchState(store, updateEntity({ id, changes }, items));
+      return of(store.itemsEntityMap()[id]);
+    },
+  })),
+
+  withHooks({
+    onInit: store => {
+      patchState(store, setAllEntities(LISTS, lists), setAllEntities(ITEMS, items));
+    },
+  }),
+);
