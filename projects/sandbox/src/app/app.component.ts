e69905b9f40b861b1446c189b4455f064eae634a
Yannick Baron <yannick.baron@thinktecture.com>
2024-03-19 13:29:03 +0100

    feat(app): toggle persistence

--- projects/sandbox/src/app/app.component.ts
+++ projects/sandbox/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, EffectRef, Signal, signal } from '@angular/core';
+import { Component, effect, EffectRef, inject, Injector, Signal, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CounterComponent } from './counter/counter.component';
 
 function restore(storageKey: string): number {
@@ -7,27 +8,44 @@ function restore(storageKey: string): number {
   return restored || 0;
 }
 
-function persist(storageKey: string, value: Signal<number>): EffectRef {
-  return effect(() => {
-    console.log('persisting', storageKey);
+function persist(storageKey: string, value: Signal<number>, injector: Injector): EffectRef {
+  return effect(
+    () => {
+      console.log('persisting', storageKey);
 
-    sessionStorage.setItem(storageKey, `${value()}`);
-  });
+      sessionStorage.setItem(storageKey, `${value()}`);
+    },
+    { injector },
+  );
 }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CounterComponent],
+  imports: [CommonModule, CounterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  readonly injector = inject(Injector);
+
   readonly counter1 = signal(restore('counter-1'));
   readonly counter2 = signal(restore('counter-2'));
   readonly counter3 = signal(restore('counter-3'));
 
-  readonly persist1 = persist('counter-1', this.counter1);
-  readonly persist2 = persist('counter-2', this.counter2);
-  readonly persist3 = persist('counter-3', this.counter3);
+  private persist1?: EffectRef;
+  private persist2?: EffectRef;
+  private persist3?: EffectRef;
+
+  managePersist(shouldPersist: boolean): void {
+    if (shouldPersist) {
+      this.persist1 = persist('counter-1', this.counter1, this.injector);
+      this.persist2 = persist('counter-2', this.counter2, this.injector);
+      this.persist3 = persist('counter-3', this.counter3, this.injector);
+    } else {
+      this.persist1?.destroy();
+      this.persist2?.destroy();
+      this.persist3?.destroy();
+    }
+  }
 }
